6c139c2bd7d85b7df1afbbf153b71895bcbba870
Sava Gerov <sava.jg@gmail.com>
2014-09-15 13:41:48 +0200

    Revert "New dependencies Fixes #234"
    This reverts commit 592f583b437540fded6e892cae9f8b08e4ab1a31.

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -1,9 +1,9 @@
 module LinkedIn
 
   module VERSION #:nodoc:
-    MAJOR = 1
-    MINOR = 0
-    PATCH = 0
+    MAJOR = 0
+    MINOR = 4
+    PATCH = 5
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
